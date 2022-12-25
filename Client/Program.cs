d36c801fcfeacc3678f0f8bc37c4d9d3493ab897
arwin-dev <arwin.dev@outlook.com>
2022-12-25 16:11:49 -0500

    best practices added

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,3 +1,6 @@
+global using invite.Shared;
+global using System.Net.Http.Json;
+global using invite.Client.Services.ProductService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using invite.Client;
@@ -7,5 +10,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IProductService,ProductService>();
 
 await builder.Build().RunAsync();
---
+++ Client/Services/ProductService/IProductService.cs
@@ -0,0 +1,8 @@
+namespace invite.Client.Services.ProductService
+{
+    public interface IProductService
+    {
+        List<Product> Products {get;set;}
+        Task GetProducts();
+    }
+}

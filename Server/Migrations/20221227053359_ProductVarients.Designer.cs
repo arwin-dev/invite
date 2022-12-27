244776468a863ebe032170a539296ddef6d3b5a5
arwin-dev <arwin.dev@outlook.com>
2022-12-27 01:13:17 -0500

    varients added - idk If we need this

--- Server/Migrations/20221227053359_ProductVarients.Designer.cs
+++ Server/Migrations/20221227053359_ProductVarients.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using invite.Server.Data;
 
@@ -10,9 +11,11 @@ using invite.Server.Data;
 namespace invite.Server.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20221227053359_ProductVarients")]
+    partial class ProductVarients
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
